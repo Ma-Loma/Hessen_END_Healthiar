74233f209fbb6cae965cc4a63bc0fbaa65010b17
Ma-Loma <59730731+Ma-Loma@users.noreply.github.com>
2025-12-16 15:30:45 +0100

    halbwegs lauffähig

---
+++ healthiar_hessen.R
@@ -0,0 +1,260 @@
+rm(list = ls())
+
+library(tidyverse)
+library(healthiar)
+library(readxl)
+library(janitor)
+library(purrr)
+
+## ETCHE Input lesen
+
+functionsETCHE <- read_excel("data/functionsETCHE.xlsx") %>%
+  mutate(ERF = str_replace_all(ERF, "threshold", as.character(threshold)))
+
+## HLNUG Expositionsdaten lesen
+
+dat_str <- read_excel(
+  "data/ULK2022StatistikGesamt_20240704.xlsx",
+  sheet = "Straßenlärm",
+  skip = 1,
+  n_max = 426
+)
+dat_flug <- read_excel(
+  "data/ULK2022StatistikGesamt_20240704.xlsx",
+  sheet = "Fluglärm",
+  skip = 1,
+  n_max = 35
+)
+
+dat_ind <- read_excel(
+  "data/ULK2022StatistikGesamt_20240704.xlsx",
+  sheet = "Industrielärm",
+  skip = 1,
+  n_max = 6
+)
+
+## Expositionsdaten schön machen
+
+lang_machen <- function(data, welche_exp) {
+  data %>%
+    janitor::clean_names() %>%
+    select(contains("gemeinde") | contains("belasteter")) %>%
+    setNames(str_replace(names(.), "_bis_[0-9]*", "")) %>%
+    pivot_longer(
+      starts_with("anzahl"),
+      names_sep = "_ab_",
+      names_to = c("metric", "L_Untergrenze"),
+      values_to = "population"
+    ) %>%
+    mutate(
+      L_Untergrenze = as.numeric(L_Untergrenze),
+      L_central = L_Untergrenze + 2,
+      .keep = "unused"
+    ) %>%
+    mutate(kreis = str_sub(gemeinde_kennziffer, start = 1L, end = 3L)) %>%
+    mutate(metric = str_remove(metric, "anzahl_belasteter_")) %>%
+    mutate(metric = str_replace_all(metric, "l_night", "lnight")) %>%
+    #    pivot_wider(names_from = metric, values_from = population) %>%
+    replace_na(list(population = 0)) %>%
+    mutate(source = welche_exp)
+}
+
+#use the source names as indicated in the ERF-Database
+dat_l <- lang_machen(dat_str, "road") %>%
+  bind_rows(lang_machen(dat_flug, "air")) %>%
+  bind_rows(lang_machen(dat_ind, "industry"))
+
+# Funktion zur Berechnung der Gesundheitsauswirkungen für eine Expositions-ERF-Kombination
+calc_geo_ar_impact <- function(dat) {
+  ifelse(dat %>%
+           select(risk_type, threshold, ERF) %>%
+           unique() %>%
+           nrow() > 1,
+         stop(
+           "Function calc_geo_ar_impact expects a data frame with a single ERF function!"
+         ),
+         NA)
+  dat %>%
+    {
+      healthiar::attribute_health(
+        approach_risk = "absolute_risk",
+        pop_exp = .$population,
+        exp_central = .$L_central,
+        cutoff_central = first(.$threshold),
+        erf_eq_central = first(.$ERF),
+        geo_id_micro = .$name_stadt_gemeinde,
+        geo_id_macro = .$kreis
+      )
+    } %>%
+    .$health_detailed %>%
+    .$results_raw
+}
+
+
+
+
+################### Visualisierungen der Expositionsdaten
+
+#Übersicht Exposition nach Kreis und Quelle
+dat_l %>%
+  ggplot(aes(x = L_central, y = population, fill = kreis)) +
+  geom_col() +
+  facet_grid(source ~ metric) +
+  labs(title = "Lärmbelastung in Hessen nach Kreisen, Quelle und Lärmmetrik", x =
+         "Lärmpegel in dB", y = "Anzahl exponierter Personen")
+ggsave("plots/exposition_hessen.png")
+
+#Übersicht Exposition im Kreis GG
+dat_l %>%
+  filter(kreis == "433") %>%
+  ggplot(aes(x = L_central, y = population, fill = name_stadt_gemeinde)) +
+  geom_col() +
+  facet_grid(source ~ metric) +
+  labs(title = "Lärmbelastung in Groß Gerau nach Kommunen, Quelle und Lärmmetrik", x =
+         "Lärmpegel in dB", y = "Anzahl exponierter Personen")
+ggsave("plots/exposition_Kreis GG.png")
+
+################### *händisches* Berechnen
+################# einzelner (!)
+################# Expositions-Wirkungen Paare
+
+
+#Zusammenführen Expositionsdaten und ERF-Funktionen 
+## (Wahrscheinlich keine gute Idee)
+dat_exp_ERF <- dat_l %>%
+  left_join(functionsETCHE,
+            by = c("source", "metric"),
+            relationship = "many-to-many") %>%
+  mutate(threshold = replace_na(threshold, 0))
+
+#Berechnung der Gesundheitsauswirkungen für Straßenlärm und hohe Lärmbelästigung
+HA_road_res <- dat_exp_ERF %>%
+  filter(source == "road") %>%
+  filter(metric == "lden") %>%
+  filter(outcome == "High noise annoyance") %>%
+  # nth(1) %>%
+  # .$ERF %>%
+  # as.character()%>%
+  {
+    calc_geo_ar_impact(.)
+  }
+
+#Berechnung der Gesundheitsauswirkungen für Fluglärm und hohe Lärmbelästigung
+HA_air_res <- dat_exp_ERF %>%
+  filter(source == "air") %>%
+  filter(metric == "lden") %>%
+  filter(outcome == "High noise annoyance") %>%
+  # nth(1) %>%
+  # .$ERF %>%
+  # as.character()%>%
+  {
+    calc_geo_ar_impact(., fkt = .$ERF)
+  }
+
+
+#Hier händisches Zusammenführen von Teilergebnisses Straßen- und Fluglärm
+
+
+#Kreis GG: 433
+#Aufteilung HA Straßenlärm im Kreis GG
+HA_road_res %>%
+  filter(geo_id_macro == 433) %>%
+  ggplot(aes(x = "", fill = geo_id_micro, y = impact)) +
+  geom_col() +
+  coord_polar(theta = "y") +
+  labs(title = "Verteilung Menschen mit durch Straßenlärm bedingten hohen Lärmbelästigung im Kreis Groß Gerau")
+ggsave("plots/HA_road_KreisGG.png")
+
+HA_all <-
+  HA_road_res %>%
+  mutate(source = "road") %>%
+  bind_rows(HA_air_res %>% mutate(source = "air"))
+
+
+HA_all %>%
+  filter(geo_id_macro == 433) %>%
+  ggplot(aes(x = source, fill = geo_id_micro, y = impact)) +
+  geom_col() +
+  labs(title = "Verteilung Menschen mit durch Straßen/FLuglärm bedingten hohen Lärmbelästigung im Kreis Groß Gerau")
+ggsave("plots/HA_FlugStraße_KreisGG.png")
+
+
+
+############################ Hier wirds wild; noch nicht abgeschlossenes Automatisierungstesten...
+
+functionsETCHE |>
+  pmap(function(outcome,
+                risk_type,
+                outcome_type,
+                population_type,
+                source,
+                ERF,
+                metric,
+                threshold,
+                reference) {
+    data.frame(erg = ERF)
+  })
+
+bla <- functionsETCHE |>
+  split(1:nrow(functionsETCHE)) |>
+  map_vec(function(exposure_outcome) {
+    dat_l %>%
+      filter(source == exposure_outcome$source,
+             metric == exposure_outcome$metric) %>%
+      healthiar::attribute_health(
+        approach_risk = "absolute_risk",
+        pop_exp = .$population,
+        exp_central = .$L_central,
+        cutoff_central = exposure_outcome$threshold,
+        erf_eq_central = exposure_outcome$ERF,
+        geo_id_micro = .$name_stadt_gemeinde,
+        geo_id_macro = .$kreis
+      ) %>%
+      .$health_detailed
+  })
+
+#
+#     data.frame(
+#         healthiar::attribute_health(
+#           approach_risk = "absolute_risk",
+#           pop_exp = 100,
+#           exp_central = 50,
+#           erf_eq_central = exposure_outcome$ERF
+#         )$health_detailed$results_raw
+#     )
+#   })
+#
+
+functionsETCHE |>
+  pmap(function(outcome,
+                risk_type,
+                outcome_type,
+                population_type,
+                source,
+                ERF,
+                metric,
+                threshold,
+                reference) {
+    data.frame(erg = ERF)
+  })
+
+
+bla <- dat_exp_ERF %>%
+  group_by(source, metric, outcome) %>%
+  group_split()
+bla %>%
+  group_keys()
+
+bla %>% reframe(
+  population_exposed = sum(population),
+  Anz_Expositionsbänder = n(),
+  fkt = first(ERF),
+  erg = calc_geo_ar_impact(.)
+)
+length(bla)
+bla[[2]] %>%
+  select(source, metric, outcome) %>%
+  unique()
+bla %>%
+  group_by(erg$approach_risk, erg$cutoff) %>%
+  summarize(n = n())
