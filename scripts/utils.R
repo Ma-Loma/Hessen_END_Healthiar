f62b39ddf79cfe1330ff706ac1e0b68a5eb3b121
Matthias Lochmann <59730731+Ma-Loma@users.noreply.github.com>
2026-05-06 14:58:08 +0200

    move extract_kreise_gemeinden to utils

--- scripts/utils.R
+++ scripts/utils.R
@@ -228,3 +228,56 @@ validate_exposure_data <- function(data) {
   
   invisible(data)
 }
+
+extract_kreise_gemeinden <- function(lines) {
+  kreise_data <- data.frame(
+    Kreis_Code = character(),
+    Kreis_Name = character(),
+    stringsAsFactors = FALSE
+  )
+  gemeinden_data <- data.frame(
+    Gemeindekennziffer = character(),
+    Gemeinde_Name = character(),
+    stringsAsFactors = FALSE
+  )
+  
+  for (line in lines) {
+    if (startsWith(line, "40")) {
+      #Zeilen, in denen Kreisdaten stehen ("Satzart 40")
+      bl_code <- substr(line, 11, 12)
+      kreis_code <- substr(line, 13, 15)
+      kreis_name <- substr(line, 23, 72)
+      kreis_name <- gsub("\\s+$", "", gsub("^\\s+", "", kreis_name))
+      kreise_data <- rbind(
+        kreise_data,
+        data.frame(
+          Bundesland_Code = bl_code,
+          Kreis_Code = kreis_code,
+          Kreis_Name = kreis_name,
+          stringsAsFactors = FALSE
+        )
+      )
+    } else if (startsWith(line, "60")) {
+      #Zeilen, in denen Gemeindedaten stehen ("Satzart 60")
+      gkz_lang <- substr(line, 11, 19)
+      gkz_kurz <- substr(line, 13, 19)
+      bl_code <- substr(line, 11, 12)
+      kreis_code <- substr(line, 13, 15)
+      gemeinde_name <- substr(line, 23, 72)
+      gemeinde_name <- gsub("\\s+$", "", gsub("^\\s+", "", gemeinde_name))
+      gemeinden_data <- rbind(
+        gemeinden_data,
+        data.frame(
+          Gemeindekennziffer_lang = gkz_lang,
+          Gemeindekennziffer_kurz = gkz_kurz,
+          Bundesland_Code = bl_code,
+          Kreis_Code = kreis_code,
+          Gemeinde_Name = gemeinde_name,
+          stringsAsFactors = FALSE
+        )
+      )
+    }
+  }
+  
+  return(list(kreise = kreise_data, gemeinden = gemeinden_data))
+}
